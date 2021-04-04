f3c805aa9317e1d552a4d7df142b4d19ca26dc6d
Brenden Wisnewski <77745573+BrendenWisnewski@users.noreply.github.com>
2021-04-04 09:20:47 -0700

    Adding in Models Classes

---
+++ Models/Models/DogParkVisit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FetchMates.Models
+{
+    public partial class DogParkVisit
+    {
+        public int DogId { get; set; }
+        public string ParkName { get; set; }
+        public DateTime DayAndTime { get; set; }
+
+        public virtual Dog Dog { get; set; }
+    }
+}
